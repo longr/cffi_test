d4208a02debe57002b32be963e05f7459fde4740
Robin Long <robin.long1@hotmail.co.uk>
2019-06-07 13:30:09 +0100

    Cleaned up build_point.py.  Added more comments based on infromation in the cffi docs.

--- point/build_point.py
+++ point/build_point.py
@@ -1,26 +1,28 @@
-#import os
+## CFFI API out-of-line implementation.
+# Test as potential for speed up by not using sources=
+# CFFI Docs, page 26.
+
 import cffi
 ffi = cffi.FFI()
 
-#with open(os.path.join(os.path.dirname(__file__), "point.h")) as f:
-#with open(os.path.join(os.path.dirname(__file__), "../src/point.h")) as f:
+# cdef() expects a single string declaring the C types, functions and
+# globals needed to use the shared object. It must be in valid C syntax.
 with open("src/point.h") as f:
     ffi.cdef(f.read())
-    
+
+
+# set_source() gives the name of the python extension module to
+# produce, and some C source code as a string. This C code needs
+# to make the declarated functions, types and globals available,
+# so it is often just the "#include".
+
 ffi.set_source("point._point",
-               # Since we are calling a fully built library directly no custom source
-               # is necessary. We need to include the .h files, though, because behind
-               # th scenes cffi generates a .c file which contains a Python-friendly
-               # wrapper around each of the functions.
                '#include "point.h"',
-               # The important thing is to include the pre-built lib in the list of
-               # libraries we are linking against:
-               #libraries=['point'],
-               #library_dirs=[os.path.dirname(__file__),'',],
-               #extra_objects=['/home/longr/Public/PyCFFI/my_cffi/_point.o'],
-               #include_dirs=['/home/longr/Public/PyCFFI/my_cffi','.','./build'],
+               # include_dirs is needed to let cffi find the .h files.
+               # their examples do not need this. Investigate?
                include_dirs=['src/'],
                sources=['src/point.c'])
                #extra_compile_args=['--std=c99'])
 
 ffi.compile(verbose=False)
+
