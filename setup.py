68f7022815ff0bdfc294f0080fab3f08a75854fe
Robin Long <robin.long1@hotmail.co.uk>
2019-06-07 13:47:04 +0100

    Added author to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(name='point',
       version='0.1',
-      #packages=find_packages(),
+      packages=find_packages(),
       description='dbader point',
       #py_modules=['point'], ?? What does this do?
       setup_requires=['cffi'],
       cffi_modules=['point/build_point.py:ffi'],
       install_requires=['cffi'],
+      author='Robin Long',
 )
