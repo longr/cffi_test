6a03698e04dd89bf9fcd71961039418e6d68ecb7
Robin Long <robin.long1@hotmail.co.uk>
2019-06-10 17:21:31 +0100

    Added more information to setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup, find_packages
 setup(name='point',
       version='0.1',
       packages=find_packages(),
-      description='dbader point',
+      description='python calling c code through cffi. supporedt by dbader point',
       #py_modules=['point'], ?? What does this do?
       setup_requires=['cffi'],
       cffi_modules=['point/build_point.py:ffi'],
+      include_dirs=['src'],
       install_requires=['cffi'],
       author='Robin Long',
+      author_email='r.l@l.com',
+      url='github.com',
+      keywords='python cffi example c-code',
+      license='need one',
 )
